0518992f4a7279028043ba6b567e49e0747227f3
han-nwin <hannguyen.win@gmail.com>
2025-09-12 22:30:24 -0500

    prb2

---
+++ Homework1/Problem2_3i.py
@@ -0,0 +1,69 @@
+import numpy as np
+
+
+def standard_subgradient_descent(X, y, iters=10**5):
+    M, n = X.shape
+
+    # feature map
+    phi = np.zeros((M, n + 1))
+    for i in range(M):
+        x1, x2 = X[i]
+        phi[i] = [x1, x2, x1**2 + x2**2]
+
+    # vector of zeros with length n+1 (ie x1, x2 -> x1, x2, x1^2, x2^2)
+    w = np.zeros(n + 1)
+    b = 0.0
+
+    inter_stops = {1, 10, 100, 10**3, 10**4, 10**5}
+    logs = []
+
+    for t in range(1, iters + 1):
+        # compute g_w g_b
+        g_w = np.zeros(n + 1)  # n=3 since φ maps to R^3
+        g_b = 0.0
+
+        # sum
+        for i in range(M):
+            f_i = w @ phi[i] + b
+            # check condition
+            if y[i] * f_i <= 0:  # misclassified
+                g_w += -y[i] * phi[i]
+                g_b += -y[i]
+
+        g_w /= -M  # * -1/M
+        g_b /= -M  # * -1/M
+
+        # Updates with step size = 1
+        w = w + g_w
+        b = b + g_b
+
+        # add to logs every iter stop
+        if t in inter_stops:
+            logs.append((t, w.copy(), b))
+
+    return logs
+
+
+# load and read the file to produce X and y
+def load_data(path):
+    # expects lines: x1,x2,y   (y in {-1, +1}), comma-separated
+    X, y = [], []
+    with open(path, "r") as f:
+        for line in f:
+            if not line.strip():
+                continue
+            x1, x2, yy = line.strip().split(",")
+            X.append([float(x1), float(x2)])
+            y.append(int(float(yy)))
+    X = np.array(X, dtype=float)  # (M,2)
+    y = np.array(y, dtype=int)  # (M,)
+    return X, y
+
+
+# run
+if __name__ == "__main__":
+    X, y = load_data("perceptron.data")
+    logs = standard_subgradient_descent(X, y)
+
+    for t, w, b in logs:
+        print(f"iter {t}: w={w}, b={b}")
